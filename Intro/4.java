0d92dc19f636782a8c665ed6b00f3ec020a7d29a
Kamil <kamil4525@gmail.com>
2018-08-12 21:33:44 +0200

    Delete 4.java

--- Intro/4.java
+++
@@ -1,8 +0,0 @@
-int adjacentElementsProduct(int[] inputArray) {
-    int m = inputArray[0]*inputArray[1];
-    for(int i=0; i<inputArray.length-1; i++){
-        int x = inputArray[i]*inputArray[i+1];
-        if(x>m) m = x;
-    } return m;
-}
-
