22e993a4de2c1b4073eeef02b3b46cb018d0471e
Kamil <kamil4525@gmail.com>
2018-08-12 21:47:01 +0200

    Create isIPv4Address.js

---
+++ Intro/Island of Knowledge/isIPv4Address.js
@@ -0,0 +1,4 @@
+function isIPv4Address(inputString) {
+ return (/^(?:(?:2(?:[0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9])\.){3}(?:(?:2([0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9]))$/.test(inputString))? true: false;
+}
+
