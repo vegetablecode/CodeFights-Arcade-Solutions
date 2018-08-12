284bde1777e222be648fa2256ca6a812beab5e22
Kamil <kamil4525@gmail.com>
2018-08-12 21:33:30 +0200

    Delete 3.java

--- Intro/3.java
+++
@@ -1,9 +0,0 @@
-bool checkPalindrome(std::string inputString) {
-    int n = inputString.length();
-    for(int i=0; i<n; i++){
-        if(inputString[i] != inputString[n-1])
-            return false;
-        else n--;
-    } return true;
-}
-
