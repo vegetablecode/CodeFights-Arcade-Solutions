5b4cc85f1f62a7f7728d735d5a23aed6e6c30d81
Kamil <kamil4525@gmail.com>
2018-08-17 20:40:03 +0200

    Create firstDigit.java

---
+++ Intro/Diving Deeper/firstDigit.java
@@ -0,0 +1,7 @@
+char firstDigit(String inputString) {
+    for(int i=0; i<inputString.length(); i++)
+        if((inputString.charAt(i)>='0')&&(inputString.charAt(i)<='9'))
+            return inputString.charAt(i);
+    return'd';
+}
+
